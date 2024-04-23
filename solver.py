b46ff34bf186bb498dc18b383fdaaf30edfa8058
Tiken9 <shushko1999@gmail.com>
2024-04-23 16:48:55 +0300

    add tsplib reader for ETSP problem

--- solver.py
+++ solver.py
@@ -1,25 +1,52 @@
-import abc 
+import abc
+import math
+import tsplib95 as lib
 import numpy as np
 
 class TSP_Problem:
     def __init__(self, matrix):
         self.matrix = matrix
 
-class ETSP_Problem(TSP_Problem):
-    def __init__(self, matrix):
-        super().__init__(matrix)
-        self.vertices = self.matrix_to_vertices()
+    @classmethod
+    def load_tsplib(cls, path: str):
+        matrix = []
+        # TODO: Реализация чтения матрицы из tsplib
+        return cls(matrix)
 
-    def matrix_to_vertices(self):
+class ETSP_Problem(TSP_Problem):
+    def __init__(self, matrix, coordinates = None):
+        super().__init__(matrix)
+        if not coordinates:
+            self.coordinates = self.matrix_to_coordinates()
+        else:
+            self.coordinates = coordinates
+
+    def matrix_to_coordinates(self):
         n = len(self.matrix)
-        vertices = []
+        coordinates = []
         for i in range(n):
             for j in range(i+1, n):
                 if self.matrix[i][j] != 0:
                     x1, y1 = i, j
                     x2, y2 = j, i
-                    vertices.append(((x1, y1), (x2, y2), self.matrix[i][j]))
-        return vertices
+                    coordinates.append(((x1, y1), (x2, y2), self.matrix[i][j]))
+        return coordinates
+
+    @classmethod
+    def load_tsplib(cls, path: str):
+        problem = lib.load(path)
+        coordinates = [problem.node_coords[i] for i in list(problem.get_nodes())]
+        n = len(coordinates)
+        matrix = np.zeros((n, n))
+        for from_counter, from_node in enumerate(coordinates):
+            for to_counter, to_node in enumerate(coordinates):
+                if from_counter == to_counter:
+                    matrix[from_counter, to_counter] = 0
+                else:
+                    # Euclidean distance
+                    matrix[from_counter, to_counter] = int(math.hypot((from_node[0] * 1000 - to_node[0] * 1000),
+                                                                      (from_node[1] * 1000 - to_node[1] * 1000)))
+        return cls(matrix, coordinates)
 
 class TSP_Solution:
     def __init__(self, problem, path, cost):
@@ -49,7 +76,7 @@ class Cluster_TSP_Solver(TSP_Solver):
         self.num_clusters = num_clusters
         self.clusters = {}
 
-    def merge_clusters(self, problems: List[TSP_Problem]) -> TSP_Problem:
+    def merge_clusters(self, problems: list[TSP_Problem]) -> TSP_Problem:
         # TODO: Реализация метода merge_clusters
         pass
 
