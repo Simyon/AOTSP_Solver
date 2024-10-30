a9605b9d3ec793ac71d772e9a00a1b5651fd8952
Simyon <semen.krassotkin@gmail.com>
2024-10-30 08:39:49 +0300

    Update solver.py
    Убрал лишние нерабочие функции. Добавил загрузку .tsp + реализовал построение серий выпуклых оболочек.

--- solver.py
+++ solver.py
@@ -2,6 +2,7 @@ import abc
 import math
 import tsplib95 as lib
 import numpy as np
+from scipy.spatial import ConvexHull   
 
 class TSP_Problem:
     def __init__(self, matrix):
@@ -10,47 +11,76 @@ class TSP_Problem:
     @classmethod
     def load_tsplib(cls, path: str):
         matrix = []
-        # TODO: Реализация чтения матрицы из tsplib
         return cls(matrix)
 
 class ETSP_Problem(TSP_Problem):
-    def __init__(self, matrix, coordinates = None):
+    def __init__(self, matrix, coordinates=None):
         super().__init__(matrix)
-        if not coordinates:
+        if coordinates is None:
             self.coordinates = self.matrix_to_coordinates()
         else:
             self.coordinates = coordinates
 
-    def matrix_to_coordinates(self):
-        n = len(self.matrix)
-        coordinates = []
-        for i in range(n):
-            for j in range(i+1, n):
-                if self.matrix[i][j] != 0:
-                    x1, y1 = i, j
-                    x2, y2 = j, i
-                    coordinates.append(((x1, y1), (x2, y2), self.matrix[i][j]))
-        return coordinates
-
     @classmethod
     def load_tsplib(cls, path: str):
         problem = lib.load(path)
+        
         coordinates = [problem.node_coords[i] for i in list(problem.get_nodes())]
         n = len(coordinates)
+        
         matrix = np.zeros((n, n))
+        
         for from_counter, from_node in enumerate(coordinates):
             for to_counter, to_node in enumerate(coordinates):
                 if from_counter == to_counter:
                     matrix[from_counter, to_counter] = 0
                 else:
-                    # Euclidean distance
-                    matrix[from_counter, to_counter] = int(math.hypot((from_node[0] * 1000 - to_node[0] * 1000),
-                                                                      (from_node[1] * 1000 - to_node[1] * 1000)))
+                    matrix[from_counter, to_counter] = cls.euclidean_distance(from_node, to_node)
+
         return cls(matrix, coordinates)
+
+    @staticmethod
+    def euclidean_distance(coord1, coord2):
+        x1, y1 = coord1
+        x2, y2 = coord2
+        distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        return distance
+
+    def extract_convex_hull(self):
+        points = np.array(self.coordinates)
+        hull = ConvexHull(points)
+        hull_points = points[hull.vertices]
+        return hull_points, hull.vertices
+
+    def extract_nested_hulls(self):
+        remaining_points = np.array(self.coordinates)
+        nested_hulls = []
+
+        while len(remaining_points) > 3:
+            hull = ConvexHull(remaining_points)
+            hull_points = remaining_points[hull.vertices]
+            nested_hulls.append(hull_points)
+            remaining_points = np.delete(remaining_points, hull.vertices, axis=0)
+
+        if len(remaining_points) > 0:
+            nested_hulls.append(remaining_points)
+
+        return nested_hulls
+
+    def combine_nested_hulls(self, nested_hulls):
+        cycle = []
+
+        # Начинаем с внешней оболочки
+        for hull in nested_hulls:
+            for point in hull:
+                if list(point) not in cycle:
+                    cycle.append(list(point))
+
+        return cycle
 
 class TSP_Solution:
     def __init__(self, problem, path, cost):
-        self.problem = problem # object of TSP_Problem
+        self.problem = problem
         self.path = path
         self.cost = cost
 
@@ -82,61 +112,25 @@ class Cluster_TSP_Solver(TSP_Solver):
 
 class ConvexHull_TSP_Solver(Cluster_TSP_Solver):
     def __init__(self, num_clusters: int):
-        super().__init__()
-        self.num_clusters = num_clusters
-        self.clusters = {}
+        super().__init__(num_clusters)
 
-    def nearest_neighbour_dot(self, point, convex_hull, forbidden_vertices = []):
-        index_min_vertex = 0
-        for i in range(len(convex_hull)):
-            if i not in forbidden_vertices and self.euclid_dist(point, convex_hull[i]) < self.euclid_dist(point, convex_hull[index_min_vertex]):
-                index_min_vertex = i
-        return index_min_vertex
+    def solve(self, problem: ETSP_Problem) -> TSP_Solution:
+        # Извлекаем вложенные выпуклые оболочки
+        nested_hulls = problem.extract_nested_hulls()
 
-    def find_closest_vertices(self, convex_hull_1, convex_hull_2, forbidden_vertices = []):
-        if len(convex_hull_2) == 1: self.nearest_neighbour_dot(convex_hull_2[0], convex_hull_1)
+        # Комбинируем вложенные выпуклые оболочки в один цикл
+        combined_cycle = problem.combine_nested_hulls(nested_hulls)
+        
+        path = []
+        for point in combined_cycle:
+            index = problem.coordinates.index(list(point))
+            path.append(index + 1)  
 
-        min_i, min_ii, min_j, min_jj = 0, 1, 0, 1
-        min_distance = self.euclid_dist(convex_hull_1[min_i], convex_hull_2[min_j]) + self.euclid_dist(convex_hull_1[min_ii], convex_hull_1[min_jj])
-        for i in range(len(convex_hull_1)):
-            c_i, c_ii = convex_hull_1[i], convex_hull_1[(i+1) % len(convex_hull_1)]
-            if c_i in forbidden_vertices or c_ii in forbidden_vertices: continue
-            for j in range(len(convex_hull_2)):
-                c_j, c_jj = convex_hull_2[j], convex_hull_2[(j+1) % len(convex_hull_2)]
-                c_distance = self.euclid_dist(c_i, c_j) + self.euclid_dist(c_ii, c_jj)
-                if min_distance > c_distance:
-                    min_i, min_ii, min_j, min_jj = i, (i+1) % len(convex_hull_1), j, (j+1) % len(convex_hull_2)
-                    min_distance = c_distance
+        # Вычисляем стоимость маршрута
+        cost = 0
+        for i in range(len(path)):
+            from_city = path[i] - 1
+            to_city = path[(i + 1) % len(path)] - 1
+            cost += problem.matrix[from_city][to_city]
 
-        return min_distance, [min_i, min_ii], [min_j, min_jj]
-
-    def solve_tsp(self, convex_hullz):
-        ch_costs = []
-        patch_costs = []
-        forbidden_vertices = {}  # changed to dict, no need for deep copy
-
-        for i in range(len(convex_hullz) - 1):
-            distance, near_1, near_2 = self.find_closest_vertices(convex_hullz[i], convex_hullz[i + 1], forbidden_vertices.keys())
-            patch_costs.append(distance)
-            forbidden_vertices[i] = near_1  # changed to tuple assignment
-            forbidden_vertices[i + 1] = near_2
-
-        for i, c_h in enumerate(convex_hullz):  # changed to enumerate
-            pts = ConvexHull(c_h)
-            ex_edge = 0
-            if i in forbidden_vertices:
-                f_A, f_B = c_h[forbidden_vertices[i][0]], c_h[forbidden_vertices[i][1]]  # changed to tuple unpacking
-                ex_edge = self.euclid_dist(f_A, f_B)
-            local_cost = self.hull_distance(pts) - ex_edge
-            ch_costs.append(local_cost)
-
-        cost = sum(ch_costs) + sum(patch_costs)
-        print("ch_costs", ch_costs)
-        print("patch_costs", patch_costs)
-        print("cost", cost)
-        return cost, ch_costs, patch_costs  # added return statement
-
-    def solve(self, problem: TSP_Problem) -> TSP_Solution:
-        cost, ch_costs, patch_costs = self.solve_tsp(convex_hullz)  # use solve_tsp to get cost
-        path = []  # TODO: generate path from convex_hullz
         return TSP_Solution(problem, path, cost)
